66196bdaf0f7ee1df28bb1a26c511c08a09cfd6a
Mary <45155816+JohnsonMary@users.noreply.github.com>
2019-01-30 14:04:17 -0500

    Update ApplicationConstants.java

--- src/test/java/com/weborders/utilities/ApplicationConstants.java
+++ src/test/java/com/weborders/utilities/ApplicationConstants.java
@@ -1,10 +1,7 @@
 package com.weborders.utilities;
 
 public class ApplicationConstants {
-    public static final String LOGIN_ERROR_MESSAGE = "Invalid Login or Password.";
-    public static final String LIST_OF_ALL_ORDERS = "List of All Orders";
-    public static final String LIST_OF_ALL_PRODUCTS = "List of Products";
-    public static final String ORDER = "Order";
-    public static final String LOGIN_PAGE_TITLE = "Web Orders Login";
-    public static final String APPLICATION_TITLE = "Web Orders";
+    
+    
+       public static final String ACCESS_ERROR_MESSAGE = "Odoo Server Error - Access Error";
 }
